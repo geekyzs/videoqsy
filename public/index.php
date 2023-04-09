8111f402cf7771ac9e5e7464ef425e4e1163a55f
杨正树 <yangzhengshu@yangzhengshudeMacBook-Pro.local>
2023-04-09 23:33:37 +0800

    update

--- public/index.php
+++ public/index.php
@@ -1,24 +1,12 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2019 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
-// [ 应用入口文件 ]
-namespace think;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-// 执行HTTP应用并响应
-$http = (new App())->http;
-
-$response = $http->run();
-
-$response->send();
-
-$http->end($response);
+set_time_limit ( 0 );
+ini_set("memory_limit","-1");
+header("Content-Type: video/mp4");
+if (!empty($_GET["url"])) {
+    $url = $_GET["url"];
+    $arr = get_headers($url, true);
+    header("content-length:" . $arr["Content-Length"]);
+    readfile($url);
+}
+?>
